692732fa5732ab5464a7cd05a12892b99b8adb9f
Gabriel Lima <gabriellimatrabalho1@gmail.com>
2025-08-13 16:37:24 -0300

    '0'

--- main.go
+++ main.go
@@ -2,12 +2,24 @@ package main
 
 import "fmt"
 
-func Hello(name string) string{
-	message := fmt.Sprintf("Ola %v. oia", name)
-	return message
-}
+// func Hello(name string) string {
+// 	message := fmt.Sprintf("Ola %v. oia", name)
+// 	return message
+// }
 
-func main(){
-	msg := Hello("Fernanda")
-	fmt.Println(msg)
+// func main() {
+// 	msg := Hello("Fernanda")
+// 	fmt.Println(msg)
+// }
+
+func main() {
+	var a int = 10
+	var b float64 = 12.99
+
+	fmt.Printf(" %d ", a)
+	fmt.Printf(" %.2f ", b)
+
+	soma := float64(a) + b
+
+	fmt.Printf("%.2f", soma)
 }
